b5774d58ab6f9cb409fd9e561e8a3a77a899e809
Brateau Quentin <quentin.brateau@ensta-bretagne.org>
2021-03-16 09:49:12 +0100

    Update

--- src/perlin_noise.py
+++ src/perlin_noise.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pnoise import PerlinNoiseFactory
 
-pnf = PerlinNoiseFactory(2, octaves=1)
+pnf = PerlinNoiseFactory(2, octaves=2)
 
 frameSize = 200
 X, Y = np.meshgrid(np.arange(0, frameSize, 1.0), np.arange(0, frameSize, 1.0))
@@ -14,29 +14,28 @@ for i in range(frameSize):
 
 dy, dx = np.gradient(current)
 
-fig, axs = plt.subplots(ncols=3, nrows=2)
-gs = axs[0, 1].get_gridspec()
-
-for ax in axs[0:, -1]:
-    ax.remove()
-for ax in axs[0:, -2]:
-    ax.remove()
-axbig = fig.add_subplot(gs[:, 1:])
+fig, axs = plt.subplots(ncols=2, nrows=2)
 
 axs[0, 0].set_title("Perlin Noise")
 axs[0, 0].imshow(current[::-1, :])
 axs[0, 0].set_aspect("equal", 'box')
 
-axbig.set_title("Contour Plot")
+axs[1, 0].set_title("Contour Plot")
 axs[1, 0].contour(X, Y, current, colors=["black"])
 axs[1, 0].contourf(X, Y, current)
 axs[1, 0].set_aspect("equal", 'box')
 
-axbig.set_title("Stream Plot")
-axbig.set_aspect("equal", 'box')
-cs = axbig.contourf(X, Y, current)
-axbig.streamplot(np.arange(0, frameSize, 1), np.arange(0, frameSize, 1), dx, dy, color='black', density=1)
-fig.colorbar(cs)
+axs[0, 1].set_title("Stream Plot")
+axs[0, 1].set_aspect("equal", 'box')
+cs = axs[0, 1].contourf(X, Y, current)
+axs[0, 1].streamplot(np.arange(0, frameSize, 1), np.arange(0, frameSize, 1), dx, dy, color='black', density=1)
+# fig.colorbar(cs)
 
-plt.tight_layout()
+axs[1, 1].set_title("Quiver Plot")
+axs[1, 1].set_aspect("equal", 'box')
+axs[1, 1].quiver(X[::4, ::4], Y[::4, ::4], dx[::4, ::4], dy[::4, ::4])
+
+
+# plt.tight_layout()
+fig.set_size_inches(7, 7)
 plt.show()
